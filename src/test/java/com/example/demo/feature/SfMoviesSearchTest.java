8c02bffc52ec8cc0985beed0d50ac30e1d9187a3
Sai To Yeung <saitoyeung@gmail.com>
2019-07-27 14:29:30 -0400

    Set up Fluentlenium feature test with example failing test

--- src/test/java/com/example/demo/feature/SfMoviesSearchTest.java
+++ src/test/java/com/example/demo/feature/SfMoviesSearchTest.java
@@ -1,4 +1,41 @@
 package com.example.demo.feature;
 
-public class SfMoviesSearchTest {
+import org.fluentlenium.adapter.junit.FluentTest;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.fluentlenium.core.hook.wait.Wait;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Wait
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SfMoviesSearchTest extends FluentTest {
+    @LocalServerPort
+    private int port;
+
+    private final String domainPrefix = "http://localhost:";
+
+    @Test
+    public void searchJourneyTest() {
+        goTo(domainPrefix + Integer.toString(port));
+        whenIEnterTheLetterA();
+        thenISeeOptionsStartingWithA();
+    }
+
+    private void thenISeeOptionsStartingWithA() {
+        List<FluentWebElement> options = $("[data-test='option']");
+        assertThat(options.size()).isGreaterThan(0);
+    }
+
+    private void whenIEnterTheLetterA() {
+        FluentWebElement searchInput = $("[data-test='search-input']").first();
+        searchInput.fill().withText("a");
+    }
 }
